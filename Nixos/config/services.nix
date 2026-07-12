52fcc968aac9fc3fd4fe10633bad5cbf654ad3c6
herauxvalle <267393638+HerauxValle@users.noreply.github.com>
2026-07-12 21:51:29 +0200

    21-51-29_12.07.2026

--- Nixos/config/services.nix
+++ Nixos/config/services.nix
@@ -5,24 +5,30 @@
   security.polkit.enable = false;
 
   # 3. Create a systemd user service to automatically manage the agent daemon
-  systemd.user.services = {
+  systemd.user = {
+    services = {
+      polkit-gnome-authentication-agent-1 = {
+        description = "polkit-gnome-authentication-agent-1";
+        wantedBy = [ "graphical-session.target" ];
+        wants = [ "graphical-session.target" ];
+        after = [ "graphical-session.target" ];
 
-    polkit-gnome-authentication-agent-1 = {
-      description = "polkit-gnome-authentication-agent-1";
-      wantedBy = [ "graphical-session.target" ];
-      wants = [ "graphical-session.target" ];
-      after = [ "graphical-session.target" ];
-
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-        Environment = "GTK_THEME=Adwaita:dark GTK_DARK_THEME=1";
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+          Restart = "on-failure";
+          RestartSec = 1;
+          TimeoutStopSec = 10;
+          Environment = "GTK_THEME=Adwaita:dark GTK_DARK_THEME=1";
+        };
       };
-
     };
 
+    settings.Manager = {
+      DefaultEnvironment = "GTK_THEME=Adwaita:dark GTK_DARK_THEME=1 XDG_CURRENT_DESKTOP=Hyprland";
+    };
   };
+
+  # Enable dconf globally out of the custom vars scope
+  programs.dconf.enable = false;
 }
