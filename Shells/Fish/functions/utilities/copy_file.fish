06565baa891553f44ccb927340c204ccddbacdc2
herauxvalle <267393638+HerauxValle@users.noreply.github.com>
2026-07-18 23:48:09 +0200

    23-48-08_18.07.2026

--- Shells/Fish/functions/utilities/copy_file.fish
+++ Shells/Fish/functions/utilities/copy_file.fish
@@ -1,5 +1,8 @@
 #&help: "Copes a file to clipboard"
-function cpf
+function cpf --description 'Copy a file object to the clipboard'
+    if test (count $argv) -eq 0
+        echo "Usage: cpf <file>"
+        return 1
+    end
     echo "file://$(realpath $argv[1])" | wl-copy -t text/uri-list
 end
-funcsave cpf
