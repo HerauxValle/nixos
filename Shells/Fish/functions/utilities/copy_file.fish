f066e31df7329cf0a2640437b4e1f0978113f668
herauxvalle <267393638+HerauxValle@users.noreply.github.com>
2026-07-19 01:38:21 +0200

    01-38-21_19.07.2026

--- Shells/Fish/functions/utilities/copy_file.fish
+++ Shells/Fish/functions/utilities/copy_file.fish
@@ -1,8 +1,11 @@
 #&help: "Copes a file to clipboard"
 function cpf --description 'Copy a file object to the clipboard'
     if test (count $argv) -eq 0
-        # Read from stdin and create temp file
-        set -l tmpfile (mktemp)
+        # Read from stdin and create temp file in Downloads (readable by all)
+        set -l tmpdir ~/Downloads
+        mkdir -p $tmpdir
+        set -l tmpfile (mktemp -p $tmpdir)
+        chmod 644 $tmpfile
         cat > $tmpfile
         echo "file://$(realpath $tmpfile)" | wl-copy -t text/uri-list
         echo "Copied: $tmpfile" >&2
