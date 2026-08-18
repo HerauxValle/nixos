64808f2ecd5c71ade9a2af7a2e17682c21d34e5d
herauxvalle <267393638+HerauxValle@users.noreply.github.com>
2026-08-18 13:35:07 +0200

    13-34-44_18.08.2026

--- Nixos/config/system/hidden-devices.nix
+++ Nixos/config/system/hidden-devices.nix
@@ -11,11 +11,11 @@
     "16dab0c7-d947-4a28-8db7-de8f2c82fb6f" # root filesystem (decrypted, label "nixos")
     "80b7960d-fb8d-4dc3-8b01-329770c6e027" # root's LUKS container (sda2, locked view)
     "88426A11426A03F2" # Windows "Basic data partition" (nvme0n1p3, unlabeled NTFS)
-    "45c06c40-9610-45ef-8d47-5938d6129e7e" # Vaults vault -- raw LUKS container (the .img duplicate)
-    "3f6f7485-0ac4-49f1-aafb-5430bc39d21f" # Davinci vault -- raw LUKS container
+    "e7a00110-6fc4-4d44-b7ce-28cbd9fe3d16" # Vaults vault -- raw LUKS container (the .img duplicate)
+    "7e796a5c-975b-47f3-ae1e-49d3500d2afc" # Davinci vault -- raw LUKS container
     "35b91a19-68aa-4856-8538-df295e12ab1d" # Tor vault -- raw LUKS container
-    "28dcfdfb-9e78-41a2-910a-4a132617e7b9" # SelfHosted vault -- raw LUKS container
-    "e8db5655-bafc-450e-8fb1-bfdc983c3ea5" # Media vault -- raw LUKS container
-    "4d682358-35a6-47c0-81e1-4f538c2942d7" # Minecraft vault -- raw LUKS container (the .img duplicate)
+    "1f06e9b3-01f3-4663-bf03-50857e075bac" # SelfHosted vault -- raw LUKS container
+    "7c0c6661-6339-4fda-8cf6-7bff2a49294e" # Media vault -- raw LUKS container
+    "fbc05a0b-f324-44bf-af90-b3027163cb84" # Minecraft vault -- raw LUKS container (the .img duplicate)
   ];
 }
