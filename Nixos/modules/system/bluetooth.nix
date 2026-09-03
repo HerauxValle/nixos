6be79fd6fbd06873bc616e3a68d6a74811767835
herauxvalle <267393638+HerauxValle@users.noreply.github.com>
2026-09-03 16:17:24 +0200

    16-17-24_03.09.2026

--- Nixos/modules/system/bluetooth.nix
+++ Nixos/modules/system/bluetooth.nix
@@ -1,4 +1,4 @@
-# &desc: "Enables hardware.bluetooth (bluez, bluetoothctl) + blueman applet."
+# &desc: "Enables hardware.bluetooth (bluez, bluetoothctl) + blueman applet -- sixaxis plugin disabled, it fights with sc-controller's own DS4 pairing and was causing repeated USB resets on the wired controller."
 
 { ... }:
 
@@ -6,6 +6,7 @@
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
+    settings.General.DisablePlugins = "sixaxis";
   };
   services.blueman.enable = true;
 }
