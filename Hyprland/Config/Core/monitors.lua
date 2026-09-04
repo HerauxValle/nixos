954d7f24a9e194e3b988232e91e2dfa0187fb5a5
herauxvalle <267393638+HerauxValle@users.noreply.github.com>
2026-09-04 14:35:11 +0200

    14-35-11_04.09.2026

--- Hyprland/Config/Core/monitors.lua
+++ Hyprland/Config/Core/monitors.lua
@@ -3,15 +3,11 @@
 -- ################
 
 -- 1. Samsung LC27G7xT (Main 2K 240Hz) - Shifted down to allow Dell to be "Higher"
--- &desc: cm=srgb keeps accurate color; sdrbrightness raised because default (1.0 -> 80 nits cap)
--- made SDR content look dim on this HDR-capable VA panel even at max OSD brightness
 hl.monitor({
-    output       = "desc:Samsung Electric Company LC27G7xT H4ZT200305",
-    mode         = "2560x1440@239.96",
-    position     = "0x360",
-    scale        = 1,
-    cm           = "srgb",
-    sdrbrightness = 1.5,
+    output   = "desc:Samsung Electric Company LC27G7xT H4ZT200305",
+    mode     = "2560x1440@239.96",
+    position = "0x360",
+    scale    = 1,
 })
 
 -- 2. Dell P2319H (Secondary FHD 60Hz) - At the top-right
